1abe8449c898adcc4671fdbb9ca134ebb6ec3f3f
takonige <84000086+takonige@users.noreply.github.com>
2021-05-13 22:35:56 +0900

    やり直し

--- discordbot.py
+++ discordbot.py
@@ -1,27 +1,21 @@
-# インストールした discord.py を読み込む
-import discord
+from discord.ext import commands
+import os
+import traceback
 
-# 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'DISCORD_BOT_TOKEN'
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
 
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
 
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(token)
